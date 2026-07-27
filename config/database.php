e75f9cd3a4880222ea9fb1cc05a6087416020865
Dante-VIQ <damalide20@gmail.com>
2026-07-27 11:00:38 +0300

    databse name update

--- config/database.php
+++ config/database.php
@@ -49,7 +49,7 @@ return [
             'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
+            'database' => env('DB_DATABASE', 'nafasi'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
@@ -118,7 +118,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'nafasi_central'), // overridden by middleware
+            'database' => env('DB_DATABASE', 'nafasi'), // overridden by middleware
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8mb4',
